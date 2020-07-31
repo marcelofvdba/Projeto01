e6d7edc8e35d579073ea126fd1da0161c758c1b6
Marcelo Vargas <marcelo.vargas@involves.com>
2020-07-30 22:10:28 -0300

    aual2

--- aula2.py
+++ aula2.py
@@ -1,6 +1,9 @@
+
 #Funções com argumentos inteiros
+
 def soma(x,y):
     return x + y
+
 
 print(soma(2,2))
 
@@ -60,3 +63,89 @@ def subtracao(num1,num2):
     return num1 - num2
 
 print(calculo(2,2,subtracao))
+
+#*args é para usar quando não sabe quantos parametros e retornam em tuplas
+#*kwargs é para argumentos nomeados e retornam dicionarios
+
+#def soma(*args):
+ #print(args)
+    #soma(2,3,3,4,4,55)
+
+def soma_total(*args):
+    total = 0
+    for numero in args:
+        total = numero + total
+        return total
+print(soma_total(25,27,29,26,28))
+
+#################kwargs
+
+def saudacoes(**kwargs):
+    print(kwargs)
+
+saudacoes(manha="bom dia", tarde="Boa Tarde",noite="Boa Noite")
+
+def saudacoes_dia(**kwargs):
+    for hora, saudacao in kwargs.items():
+        print(f"Durante a {hora} dizemos {saudacao} ")
+
+saudacoes_dia(manha = "Bom dia", tarde="Boa tarde", noite="Boa noite")
+
+
+####Funções decoradoras####
+
+def master(msg):
+    def imprime():
+        print("Essa é função principal. ")
+        msg() #precisa dizer que este é um argumento vazio
+    return imprime
+
+
+def chama_funcao():
+    print("Essa é a Segunda Função. ")
+
+chama_funcao()
+
+##OUtro exemplo
+
+def decoradora(valor):
+    def imprime(*args):
+        print("Multiplicação efetuada")
+        return valor(*args)
+    return imprime
+
+@decoradora
+def multiplica(a,b):
+    return a * b
+
+print(multiplica(2,3))
+
+
+
+contador = 1
+def contar_acessos(funcao_decorada):
+    def nova_funcc(*args,**kwargs):
+        global contador
+        contador += 1
+        return funcao_decorada(*args,**kwargs)
+    return nova_funcc
+
+@contar_acessos
+def somar(a, b):
+    return a + b
+
+print(contador)
+print(somar(2,2))
+print(contador)
+
+
+"""Crie uma função que faça a soma de 3 números e imprima em tela o resultado da
+soma.
+Crie um programa onde a função irá validar o nome e o CPF da pessoa estão
+corretos baseados nos dados apresentados. Valide com retornos em tela se o CPF
+está correto se o nome está correto.
+Crie funções com argumentos *args e **kwargs e faça teste de saida dos
+argumentos e execute o iterador for nos elementos.
+Crie 2 funções onde 1 será a função decoradora e a outra irá receber a função
+decorada. Teste pelo método decorado e pelo método através de variáveis
+"""
